162acdab0b2e4de486dfafbfed0e994091de00ed
nchikun <donkonchi@gmail.com>
2018-11-16 01:21:13 +0900

    Add user microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,15 +1,12 @@
-# 静的ページは基本的にメソッドの中身は書かない
 class StaticPagesController < ApplicationController
 
-  # /static_pages/homeというGETリクエストに対して実行される
-  # homeメソッドではhome.html.erbのViewで使用する変数を定義する（StaticPageだから定義しない？）
-  # homeメソッドの実行後にhome.html.erbがブラウザで表示される
   def home
+    if logged_in?
+      @micropost = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
-  # /static_pages/helpというGETリクエストに対して実行される
-  # helpメソッドではhelp.html.erbのViewで使用する変数を定義する（StaticPageだから定義しない？）
-  # helpメソッドの実行後にhelp.html.erbがブラウザで表示される
   def help
   end
 
